c80ffc7777b801007cbf533654eb74a01597be0d
amit nissan <lioamit0105@gmail.com>
2025-02-09 16:54:00 +0200

    added q1.sh answers

--- q1.sh
+++ q1.sh
@@ -1,1 +1,12 @@
 This is q1 answer
+
+#a
+awk 'END {print NR}' aliceinwonderland.txt 
+#b
+awk '{for (i=1; i<=NF; i++) if ($i=="Alice") count++} END {print count}' aliceinwonderland.txt 
+#c
+ awk '{for (i=1; i<=NF; i++) a[$i]++} END {for (k in a) if(a[k]==1) print k}' aliceinwonderland.txt 
+#d
+awk '{for (i=2; i<=NF-1; i++) a[$i]++} END {for (k in a) print a[k],k}' aliceinwonderland.txt | sort -n | tail -5 
+#e
+awk '{for (i=1; i<=NF; i++) {sum=sum+length($i); count++}} END {print sum/count}' aliceinwonderland.txt y
